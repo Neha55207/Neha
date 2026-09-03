ddc0994dc25781db52fc8af05c5a2df59a51da78
Neha55207 <nehabb31@gmail.com>
2026-09-03 11:41:14 +0530

    Add age check to print adult status

--- git.py
+++ git.py
@@ -4,3 +4,8 @@ age = input("Enter your age: ")
 print("Hello", name)
 print("Your age is", age)
 print("Welcome to GitHub!")
+
+if int(age) >= 18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
