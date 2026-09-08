89db3260ebfcd388e3f1c6b438b87835c3467efe
Neha <nehabb31@gmail.com>
2026-09-08 13:50:20 +0530

    testing git.py changes

--- git.py
+++ git.py
@@ -4,7 +4,7 @@ age = input("Enter your age: ")
 print("Hello", name)
 print("Your age is", age)
 print("Hello from test branch")
-
+print("Nehas test")
 if int(age) >= 18:
     print("You are an adult.")
 else:
